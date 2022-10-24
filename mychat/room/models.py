3ec94980d955f357ac542ef8dd703752a62705b2
roman7586 <1xcanonx1>
2022-10-24 23:04:59 +0300

    МОдель пользователя, аватар

--- mychat/room/models.py
+++ mychat/room/models.py
@@ -1,3 +1,19 @@
+from tkinter import _ImageSpec
 from django.db import models
+from requests import options
 
 # Create your models here.
+
+def user_directory_path(instance, filename): # Создание папка с контентом под каждого пользователя
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class ChatUser (User):
+    avatar = models.ImageField(upload_to=user_directory_path)
+    avatar_thumbnail = ImageSpecField (source = 'avatar', 
+        processors = [ResizeToFill(150,150)],
+        format = 'JPEG',
+        options = {'quality': 100})
+
+class Room (models.Model):
+    creater = models.ForeignKey(ChatUser, verbose_name='Создатель', on_delete=models.CASCADE)
+    
